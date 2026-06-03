cb1115d4bdc0671c8420e7375206c1c60543b74d
Naotaro <sample53534@gmail.com>
2026-06-04 01:22:11 +0900

    Add hero section and fade-in animation

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ console.log(hamburgur);
 hamburgur.addEventListener("click", function(){
     hamburgur.classList.toggle("active");
 });
+window.addEventListener("load", () => {
+    const titles = document.querySelector(".titles");
+    titles.classList.add("show");
+});
